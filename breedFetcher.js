73db8ebc33c53ed5f905deecd50856799bb1dcc3
malak <malak-y@live.com>
2019-12-05 21:19:32 +0000

    Testing Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,16 @@
 const request = require('request');
-let args = process.argv.slice(2);
-request('https://api.thecatapi.com/v1/breeds/search?q=' + args, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('error:', error);
-    console.log('statusCode:', response && response.statusCode);
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-    console.log(typeof data);
-  }
-});
+
+
+const fetchBreedDescription = function (breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+
+      const data = JSON.parse(body);
+      callback(null, data[0].description.trim());
+
+    }
+  });
+};
+module.exports = { fetchBreedDescription };
